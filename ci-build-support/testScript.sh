3be927d3aa6221150d603cd698cec3a267e46bf4
Abhilash Issac <66210365+abhilashissac@users.noreply.github.com>
2020-11-30 23:35:33 +0530

    Substitute 'testSchemeName' & 'FrameworkName' with $1 and $2 respectively

--- ci-build-support/testScript.sh
+++ ci-build-support/testScript.sh
@@ -11,7 +11,7 @@ export LC_CTYPE=en_US.UTF-8
 
 xcodebuild test \
         -workspace ${"PLF-IOS-WORKSPACE.xcworkspace"} \
-        -scheme ${testSchemeName} CLANG_WARN_DOCUMENTATION_COMMENTS='NO'\
+        -scheme $1 CLANG_WARN_DOCUMENTATION_COMMENTS='NO'\
         -destination \'platform=iOS Simulator,name=iPhone 8,OS=latest\' \
         -UseModernBuildSystem='YES'\
         -resultBundlePath ${resultBundlePath} \
@@ -21,26 +21,26 @@ mkdir -p "Coverage"
 if [ -d "${{github.workspace}}/Source/DerivedData/PLF-IOS-WORKSPACE/Build/ProfileData/4B0465A0-EB0F-47D0-97A6-15FCB16B26E4" ]
     then
         echo "Directory exists in iet-iOS-blr-001"
-            if [ "$testSchemeName" == "AppFramework" ]
+            if [ "$1" == "AppFramework" ]
                 then
-                    xcrun llvm-cov show ${{github.workspace}}/Source/DerivedData/PLF-IOS-WORKSPACE/Build/products/Debug-iphonesimulator/${testSchemeName}.app/${testSchemeName} -instr-profile=${WORKSPACE}/Source/DerivedData/PLF-IOS-WORKSPACE/Build/ProfileData/4B0465A0-EB0F-47D0-97A6-15FCB16B26E4/Coverage.profdata -arch=x86_64 &> ${WORKSPACE}/Source/Coverage/llvm-cov-show-AppFramework_Profile.txt
-            elif [ "$testSchemeName" == "AppInfraDev" ] 
+                    xcrun llvm-cov show ${{github.workspace}}/Source/DerivedData/PLF-IOS-WORKSPACE/Build/products/Debug-iphonesimulator/$1.app/$1 -instr-profile=${{github.workspace}}/Source/DerivedData/PLF-IOS-WORKSPACE/Build/ProfileData/4B0465A0-EB0F-47D0-97A6-15FCB16B26E4/Coverage.profdata -arch=x86_64 &> ${{github.workspace}}/Source/Coverage/llvm-cov-show-AppFramework_Profile.txt
+            elif [ "$1" == "AppInfraDev" ] 
                 then
-                    xcrun llvm-cov show ${{github.workspace}}/Source/DerivedData/PLF-IOS-WORKSPACE/Build/products/Debug-iphonesimulator/${frameworkName}.framework/${frameworkName} -instr-profile=${WORKSPACE}/Source/DerivedData/PLF-IOS-WORKSPACE/Build/ProfileData/4B0465A0-EB0F-47D0-97A6-15FCB16B26E4/Coverage.profdata -arch=x86_64 &> ${WORKSPACE}/Source/Coverage/llvm-cov-show-${testSchemeName}_Profile.txt
+                    xcrun llvm-cov show ${{github.workspace}}/Source/DerivedData/PLF-IOS-WORKSPACE/Build/products/Debug-iphonesimulator/$2.framework/$2 -instr-profile=${{github.workspace}}/Source/DerivedData/PLF-IOS-WORKSPACE/Build/ProfileData/4B0465A0-EB0F-47D0-97A6-15FCB16B26E4/Coverage.profdata -arch=x86_64 &> ${{github.workspace}}/Source/Coverage/llvm-cov-show-$1_Profile.txt
             else
-                xcrun llvm-cov show ${{github.workspace}}/Source/DerivedData/PLF-IOS-WORKSPACE/Build/products/Debug-iphonesimulator/${testSchemeName}.framework/${testSchemeName} -instr-profile=${WORKSPACE}/Source/DerivedData/PLF-IOS-WORKSPACE/Build/ProfileData/4B0465A0-EB0F-47D0-97A6-15FCB16B26E4/Coverage.profdata -arch=x86_64 &> ${WORKSPACE}/Source/Coverage/llvm-cov-show-${testSchemeName}_Profile.txt
+                xcrun llvm-cov show ${{github.workspace}}/Source/DerivedData/PLF-IOS-WORKSPACE/Build/products/Debug-iphonesimulator/$1.framework/$1 -instr-profile=${{github.workspace}}/Source/DerivedData/PLF-IOS-WORKSPACE/Build/ProfileData/4B0465A0-EB0F-47D0-97A6-15FCB16B26E4/Coverage.profdata -arch=x86_64 &> ${{github.workspace}}/Source/Coverage/llvm-cov-show-$1_Profile.txt
             fi
 elif [ -d "${{github.workspace}}/Source/DerivedData/PLF-IOS-WORKSPACE/Build/ProfileData/BF86F4D9-4F86-4F54-AB68-562AEEE9AE57" ]
     then
         echo "Directory exists in iet-iOS-blr-002"
-            if [ "$testSchemeName" == "AppFramework" ]
+            if [ "$1" == "AppFramework" ]
                 then
-                    xcrun llvm-cov show ${{github.workspace}}/Source/DerivedData/PLF-IOS-WORKSPACE/Build/products/Debug-iphonesimulator/${testSchemeName}.app/${testSchemeName} -instr-profile=${WORKSPACE}/Source/DerivedData/PLF-IOS-WORKSPACE/Build/ProfileData/BF86F4D9-4F86-4F54-AB68-562AEEE9AE57/Coverage.profdata -arch=x86_64 &> ${WORKSPACE}/Source/Coverage/llvm-cov-show-AppFramework_Profile.txt
-            elif [ "$testSchemeName" == "AppInfraDev" ] 
+                    xcrun llvm-cov show ${{github.workspace}}/Source/DerivedData/PLF-IOS-WORKSPACE/Build/products/Debug-iphonesimulator/$1.app/$1 -instr-profile=${{github.workspace}}/Source/DerivedData/PLF-IOS-WORKSPACE/Build/ProfileData/BF86F4D9-4F86-4F54-AB68-562AEEE9AE57/Coverage.profdata -arch=x86_64 &> ${{github.workspace}}/Source/Coverage/llvm-cov-show-AppFramework_Profile.txt
+            elif [ "$1" == "AppInfraDev" ] 
                 then
-                    xcrun llvm-cov show ${{github.workspace}}/Source/DerivedData/PLF-IOS-WORKSPACE/Build/products/Debug-iphonesimulator/${frameworkName}.framework/${frameworkName} -instr-profile=${WORKSPACE}/Source/DerivedData/PLF-IOS-WORKSPACE/Build/ProfileData/BF86F4D9-4F86-4F54-AB68-562AEEE9AE57/Coverage.profdata -arch=x86_64 &> ${WORKSPACE}/Source/Coverage/llvm-cov-show-${testSchemeName}_Profile.txt
+                    xcrun llvm-cov show ${{github.workspace}}/Source/DerivedData/PLF-IOS-WORKSPACE/Build/products/Debug-iphonesimulator/$2.framework/$2 -instr-profile=${{github.workspace}}/Source/DerivedData/PLF-IOS-WORKSPACE/Build/ProfileData/BF86F4D9-4F86-4F54-AB68-562AEEE9AE57/Coverage.profdata -arch=x86_64 &> ${{github.workspace}}/Source/Coverage/llvm-cov-show-$1_Profile.txt
             else
-                xcrun llvm-cov show ${{github.workspace}}/Source/DerivedData/PLF-IOS-WORKSPACE/Build/products/Debug-iphonesimulator/${testSchemeName}.framework/${testSchemeName} -instr-profile=${WORKSPACE}/Source/DerivedData/PLF-IOS-WORKSPACE/Build/ProfileData/BF86F4D9-4F86-4F54-AB68-562AEEE9AE57/Coverage.profdata -arch=x86_64 &> ${WORKSPACE}/Source/Coverage/llvm-cov-show-${testSchemeName}_Profile.txt
+                xcrun llvm-cov show ${{github.workspace}}/Source/DerivedData/PLF-IOS-WORKSPACE/Build/products/Debug-iphonesimulator/$1.framework/$1 -instr-profile=${{github.workspace}}/Source/DerivedData/PLF-IOS-WORKSPACE/Build/ProfileData/BF86F4D9-4F86-4F54-AB68-562AEEE9AE57/Coverage.profdata -arch=x86_64 &> ${{github.workspace}}/Source/Coverage/llvm-cov-show-$1_Profile.txt
             fi
 else    
     echo "Error: Directory does not exist in both the nodes"
