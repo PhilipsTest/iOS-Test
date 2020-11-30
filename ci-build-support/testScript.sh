97b912937c9fd37e75ce4c14cf5eed1aebe7a7de
Abhilash Issac <66210365+abhilashissac@users.noreply.github.com>
2020-12-01 00:53:26 +0530

    Removed 'Bad Substitution'

--- ci-build-support/testScript.sh
+++ ci-build-support/testScript.sh
@@ -4,13 +4,13 @@ export PATH=/.rbenv/shims:/usr/local/bin/xcpretty/bin:/usr/local/bin:/usr/bin:/b
 killall Simulator || true
 xcrun simctl erase all || true
 
-cd ${"Source"}
+cd Source
 
 #Set XCPretty output format
 export LC_CTYPE=en_US.UTF-8
 
 xcodebuild test \
-        -workspace ${"PLF-IOS-WORKSPACE.xcworkspace"} \
+        -workspace PLF-IOS-WORKSPACE.xcworkspace \
         -scheme $1 CLANG_WARN_DOCUMENTATION_COMMENTS='NO'\
         -destination \'platform=iOS Simulator,name=iPhone 8,OS=latest\' \
         -UseModernBuildSystem='YES'\
@@ -18,7 +18,7 @@ xcodebuild test \
         | xcpretty --report junit --report html
 
 mkdir -p "Coverage"
-if [ -d "${{github.workspace}}/Source/DerivedData/PLF-IOS-WORKSPACE/Build/ProfileData/4B0465A0-EB0F-47D0-97A6-15FCB16B26E4" ]
+if [ -d ${{github.workspace}}/Source/DerivedData/PLF-IOS-WORKSPACE/Build/ProfileData/4B0465A0-EB0F-47D0-97A6-15FCB16B26E4 ]
     then
         echo "Directory exists in iet-iOS-blr-001"
             if [ "$1" == "AppFramework" ]
@@ -30,7 +30,7 @@ if [ -d "${{github.workspace}}/Source/DerivedData/PLF-IOS-WORKSPACE/Build/Profil
             else
                 xcrun llvm-cov show ${{github.workspace}}/Source/DerivedData/PLF-IOS-WORKSPACE/Build/products/Debug-iphonesimulator/$1.framework/$1 -instr-profile=${{github.workspace}}/Source/DerivedData/PLF-IOS-WORKSPACE/Build/ProfileData/4B0465A0-EB0F-47D0-97A6-15FCB16B26E4/Coverage.profdata -arch=x86_64 &> ${{github.workspace}}/Source/Coverage/llvm-cov-show-$1_Profile.txt
             fi
-elif [ -d "${{github.workspace}}/Source/DerivedData/PLF-IOS-WORKSPACE/Build/ProfileData/BF86F4D9-4F86-4F54-AB68-562AEEE9AE57" ]
+elif [ -d ${{github.workspace}}/Source/DerivedData/PLF-IOS-WORKSPACE/Build/ProfileData/BF86F4D9-4F86-4F54-AB68-562AEEE9AE57 ]
     then
         echo "Directory exists in iet-iOS-blr-002"
             if [ "$1" == "AppFramework" ]
