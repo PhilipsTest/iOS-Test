a6ab2c4f6f03ea644a66847c9f83ef2190d2facd
Abhilash Issac <66210365+abhilashissac@users.noreply.github.com>
2021-01-15 01:40:30 +0530

    Add files via upload

---
+++ ci-build-support/getUpdatedComponents.groovy
@@ -0,0 +1,29 @@
+def updatedComponents = ["rap"]
+
+updatedComponents = getUpdatedComponents()
+
+def getUpdatedComponents() {
+    releaseBranchPattern = "release/platform_*"
+    developBranchPattern = "develop"
+    if (BranchName =~ /${releaseBranchPattern}/ || BranchName == developBranchPattern) {
+        return ["rap"]
+    }
+    commit_to_compare_with = "${env.GIT_PREVIOUS_SUCCESSFUL_COMMIT}"
+    if (env.GIT_PREVIOUS_SUCCESSFUL_COMMIT == null) {
+        commit_to_compare_with = "origin/develop"
+    }
+    scriptValue = "git diff --name-only " + commit_to_compare_with
+    scriptResult = sh (script: scriptValue, returnStdout: true).trim()
+    changed_files = scriptResult.tokenize('\n')
+    changed_files.remove("Jenkinsfile")
+    changed_files.remove("ci-build-support/Versions.rb")
+    echo "Updated files:" + changed_files
+    def components = []
+    changed_files.each {
+        pathComponents = it.split("/")
+        if (pathComponents.length > 1) {
+            components << pathComponents[1]
+        }
+    }
+    return components
+}
